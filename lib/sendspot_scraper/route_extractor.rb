881641021b9d7514dc5c2791879092fbc72dd3ce
Paul Salaets <psalaets@gmail.com>
2013-03-07 17:55:25 -0500

    rename

--- lib/sendspot_scraper/route_extractor.rb
+++ lib/sendspot_scraper/route_extractor.rb
@@ -21,15 +21,15 @@ module SendspotScraper
     private
 
     def extract_name(html)
-      text_nodes = html.xpath('//tr[@id="body"]/td[1]/p[1]/strong[1]/child::text()')
-      text = text_nodes.map(&:text).join
+      name_grade_text_nodes = html.xpath('//tr[@id="body"]/td[1]/p[1]/strong[1]/child::text()')
+      text = name_grade_text_nodes.map(&:text).join
 
       text.split('-').first.strip
     end
 
     def extract_grade(html)
-      text_nodes = html.xpath('//tr[@id="body"]/td[1]/p[1]/strong[1]/child::text()')
-      text = text_nodes.map(&:text).join
+      name_grade_text_nodes = html.xpath('//tr[@id="body"]/td[1]/p[1]/strong[1]/child::text()')
+      text = name_grade_text_nodes.map(&:text).join
 
       text.split('-').last.strip
     end

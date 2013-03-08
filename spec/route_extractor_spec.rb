96239234ce43d36c6ba09720ad4e5473e3c8f176
Paul Salaets <psalaets@gmail.com>
2013-03-07 22:40:25 -0500

    reword example descriptions

--- spec/route_extractor_spec.rb
+++ spec/route_extractor_spec.rb
@@ -14,37 +14,37 @@ module SendspotScraper
         @html = nil
       end
 
-      it("should return route with name from html") do
+      it("should pull route name from html") do
         route = @extractor.extract(@html)
 
         route.name.should eq('Pity The Fool')
       end
 
-      it("should return route with grade from html") do
+      it("should pull grade from html") do
         route = @extractor.extract(@html)
 
         route.grade.should eq('5.10b')
       end
 
-      it("should return route with setter from html") do
+      it("should pull setter from html") do
         route = @extractor.extract(@html)
 
         route.set_by.should eq('Ryan Blah')
       end
 
-      it("should return route with location from html") do
+      it("should pull location from html") do
         route = @extractor.extract(@html)
 
         route.location.should eq('Rockville')
       end
 
-      it("should return route with gym from html") do
+      it("should pull gym from html") do
         route = @extractor.extract(@html)
 
         route.gym.should eq('Earth Treks')
       end
 
-      it("should return route with climb types from html") do
+      it("should pull climb types from html") do
         route = @extractor.extract(@html)
 
         Set.new(route.types).should eq(Set.new(['Lead', 'Top-Rope']))

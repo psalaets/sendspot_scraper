ff9eb9e1c2239cb243b146743d8e91507361272a
Paul Salaets <psalaets@gmail.com>
2013-03-08 11:25:25 -0500

    another level of context for when html has all required fields

--- spec/route_extractor_spec.rb
+++ spec/route_extractor_spec.rb
@@ -6,41 +6,61 @@ module SendspotScraper
     context "#extract" do
       before :each do
         @extractor = RouteExtractor.new
-        @html = SendspotScraper.route_details_html
+        @fully_populated_html = SendspotScraper.route_details_html
       end
 
       after :each do
         @extractor = nil
-        @html = nil
+        @fully_populated_html = nil
       end
 
-      it("should pull route name from html") do
-        route = @extractor.extract(@html)
+      context "when all required fields are available in html" do
+        it("should pull route name from html") do
+          route = @extractor.extract(@fully_populated_html)
 
-        route.name.should eq('Pity The Fool')
-      end
-
-      it("should pull grade from html") do
-        route = @extractor.extract(@html)
-
-        route.grade.should eq('5.10b')
-      end
-
-      context "when setter has nickname" do
-        it("should pull setter nick from html") do
-          route = @extractor.extract(@html)
-
-          route.set_by.should eq('Ryan B')
+          route.name.should eq('Pity The Fool')
         end
-      end
 
-      context "when setter doesn't have nickname" do
-        it("should pull setter name from html") do
-          html = SendspotScraper.route_details_html(:has_setter_nick => false)
+        it("should pull grade from html") do
+          route = @extractor.extract(@fully_populated_html)
 
-          route = @extractor.extract(html)
+          route.grade.should eq('5.10b')
+        end
 
-          route.set_by.should eq('Ryan Blah')
+        context "when setter has nickname" do
+          it("should pull setter nick from html") do
+            route = @extractor.extract(@fully_populated_html)
+
+            route.set_by.should eq('Ryan B')
+          end
+        end
+
+        context "when setter doesn't have nickname" do
+          it("should pull setter name from html") do
+            html = SendspotScraper.route_details_html(:has_setter_nick => false)
+
+            route = @extractor.extract(html)
+
+            route.set_by.should eq('Ryan Blah')
+          end
+        end
+
+        it("should pull location from html") do
+          route = @extractor.extract(@fully_populated_html)
+
+          route.location.should eq('Rockville')
+        end
+
+        it("should pull gym from html") do
+          route = @extractor.extract(@fully_populated_html)
+
+          route.gym.should eq('Earth Treks')
+        end
+
+        it("should pull climb types from html") do
+          route = @extractor.extract(@fully_populated_html)
+
+          Set.new(route.types).should eq(Set.new(['Lead', 'Top-Rope']))
         end
       end
 
@@ -50,24 +70,6 @@ module SendspotScraper
 
           expect { @extractor.extract(html) }.to raise_error(DataExtractionError, "route.setter")
         end
-      end
-
-      it("should pull location from html") do
-        route = @extractor.extract(@html)
-
-        route.location.should eq('Rockville')
-      end
-
-      it("should pull gym from html") do
-        route = @extractor.extract(@html)
-
-        route.gym.should eq('Earth Treks')
-      end
-
-      it("should pull climb types from html") do
-        route = @extractor.extract(@html)
-
-        Set.new(route.types).should eq(Set.new(['Lead', 'Top-Rope']))
       end
     end
   end

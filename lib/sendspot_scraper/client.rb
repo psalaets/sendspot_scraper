45ae4c6d2d0e3cd6b437ec8d90985451fe3e1bdf
Paul Salaets <psalaets@gmail.com>
2013-04-05 17:34:49 -0400

    client hits rss url now

--- lib/sendspot_scraper/client.rb
+++ lib/sendspot_scraper/client.rb
@@ -1,102 +1,38 @@
 require 'open-uri'
 
 module SendspotScraper
-  # Sendspot client that retrieves html.
+  # Sendspot client that retrieves route feed xml.
   class Client
     # Public: Create a new client.
     #
-    # options - Hash of Client config options which are:
-    #
-    # :gym         - Required, gym name as abbreviated by send spot, e.g.
-    #                'earthtreks'.
-    # :location_id - Required, location id typically used by send spot as 'gid',
-    #                e.g. 1.
-    # :verbose     - Optional, true to print urls visited, defaults to false.
-    def initialize(options = {})
-      raise ArgumentError.new(':gym is required') unless options[:gym]
-      raise ArgumentError.new(':location_id is required') unless options[:location_id]
+    # location_ids - Location ids or Array of location ids to check for new
+    #                routes. These should be the ids used in sendspot as 'gid',
+    #                e.g. 1. Must contain at least one id.
+    def initialize(*location_ids)
+      location_ids = location_ids.flatten
+      raise ArgumentError.new('at least one location id is required') if location_ids.empty?
 
-      @gym = options[:gym]
-      @location_id = options[:location_id]
-      @verbose = options[:verbose] || false
+      @location_ids = location_ids
     end
 
-    # Public: Gets html of routes search page.
+    # Public: Gets xml of recent routes xml.
     #
-    # days_old - How many days worth of routes to include in html.
+    # days_old - How many days worth of routes to include in xml.
     #
-    # Returns String of route search page html, newest routes first.
+    # Returns String of route rss feed xml, newest routes first.
     def recent_routes(days_old)
-      visit(recent_routes_uri(days_old))
-    end
-
-    # Public: Gets html of route details page.
-    #
-    # id - Route id to get details of.
-    #
-    # Returns String of route details html.
-    def route_details(id)
-      visit(route_uri(id))
-    end
-
-    def route_url(id)
-      route_uri(id).to_s
-    end
-
-    def id_from_route_url(url)
-      query_string = URI(url).query
-
-      pairs = {}
-      query_string.split('&').each do |pair|
-        name, value = pair.split('=')
-        pairs[name] = value
-      end
-      pairs['rid']
+      recent_routes_uri(days_old).read
     end
 
     private
 
-    def visit(uri)
-      puts "Visiting #{uri}" if @verbose
-      uri.read
-    end
-
     def recent_routes_uri(days_old)
-      date_range = {
-        :start_date => (Date.today - days_old).to_s,
-        :end_date => Date.today.to_s
-      }
-
-      query_string = route_search_query_string(date_range)
-      URI("#{base_url}routes?#{query_string}")
+      gids = @location_ids.join(',')
+      URI("#{rss_url}?gids=#{gids}&days_old=#{days_old}")
     end
 
-    def route_search_query_string(params = {})
-      defaults = {
-        :searching    => 1,
-        :string       => '',
-        :rt_lead      => 'on',
-        :rt_toprope   => 'on',
-        :rt_boulder   => 'on',
-        :start_grade  => 0,
-        :end_grade    => 0,
-        :gym          => @location_id,
-        :setter       => 0,
-        # Sort by set date, newest first
-        :sort_by      => 'dateup',
-        :sort_order   => 'descending'
-      }
-
-      query = defaults.merge(params)
-      query.map { |k, v| "#{k}=#{v}" }.join('&')
-    end
-
-    def route_uri(id)
-      URI("#{base_url}route?rid=#{id}")
-    end
-
-    def base_url
-      "https://secure.thesendspot.com/#{@gym}/"
+    def rss_url
+      "https://secure.thesendspot.com/vc/rss"
     end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,36 +2,9 @@ require 'spec_helper'
 
 module SendspotScraper
   describe Client do
-    it "should require :gym" do
-      expect { Client.new(:location_id => 5) }.to raise_error(ArgumentError)
-    end
-
-    it "should require :location_id" do
-      expect { Client.new(:gym => 'blah') }.to raise_error(ArgumentError)
-    end
-
-    it "should generate route url from route id" do
-      client = Client.new(:gym => 'gym', :location_id => 5)
-
-      url = client.route_url('20')
-
-      url.should eq('https://secure.thesendspot.com/gym/route?rid=20')
-    end
-
-    it "should pull route id from full route url" do
-      client = Client.new(:gym => 'gym', :location_id => 5)
-
-      id = client.id_from_route_url('https://secure.thesendspot.com/gym/route?rid=20')
-
-      id.should eq('20')
-    end
-
-    it "should pull route id from relative route url" do
-      client = Client.new(:gym => 'gym', :location_id => 5)
-
-      id = client.id_from_route_url('./route?rid=21')
-
-      id.should eq('21')
+    it "should require at least one location id" do
+      expect { Client.new() }.to raise_error(ArgumentError)
+      expect { Client.new([]) }.to raise_error(ArgumentError)
     end
   end
 end
